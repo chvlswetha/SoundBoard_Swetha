69d0967d7f8e805d554ffb542b0b31f27e2eb101
chvlswetha <chvl.swetha@gmail.com>
2022-06-13 01:34:31 -0700

    Initial commit

---
+++ Model/Sound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Model
+{
+      public enum SoundCategroy
+        {
+            Animals,
+           Cartoons,
+           Taunts,
+           Warnings
+        }
+    internal class Sound
+    {
+        public String Name { get; set; }
+        public SoundCategroy Categroy { get; set; }
+
+        public String AudioFile { get; set; }
+
+        public String ImageFile { get; set; }
+    }
+}
